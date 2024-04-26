ea0929937162fc1848ae5b38ad9997af7d06150f
Flora <mariaflorarenata12@gmail.com>
2024-04-26 16:18:48 +0700

    refactor: attempt goroutine (untested)

--- src/backend/main.go
+++ src/backend/main.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"runtime"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/PuerkitoBio/goquery"
@@ -173,7 +175,13 @@ func BFS(goal string) {
 	fmt.Printf("Time elapsed: %s", end);
 }
 
-func DFS(start *TreeNode, curPath []*TreeNode, depth int, goal string) bool {
+func boolHelper(b bool) *bool {
+	return &b
+}
+
+func DLS(wg *sync.WaitGroup, start *TreeNode, curPath []*TreeNode, depth int, goal string, found *bool) {
+	defer wg.Done()
+
 	if start.Root == goal { //menemukan goal
 		var path []string
 		path = start.GetPath(path)
@@ -184,34 +192,34 @@ func DFS(start *TreeNode, curPath []*TreeNode, depth int, goal string) bool {
 			fmt.Printf("%d. %s\n", i, GetTitle(page))
 			i++
 		}
-		return true
-	}
-
-	if visited[start.Root] == 0 { //menandai current node sudah dikunjungi
-		visited[start.Root] = 1
-	}
-
-	if (depth <= 0) { //sudah mencapai kedalaman maksimum tetapi tidak menemukan goal
-		return false
-	}
-	
-	start.AddChildren()
-	for _, v := range start.Children { //melanjutkan DFS pada children dari node start
-		found = DFS(v, append(curPath, v), depth-1, goal)
-		if (found) {
-			return true
+		found = boolHelper(true)
+	} else if (depth <= 0){ //sudah mencapai kedalaman maksimum tetapi tidak menemukan goal
+		found = boolHelper(false)
+	} else { //lanjutkan pencarian
+		if visited[start.Root] == 0 { //menandai current node sudah dikunjungi
+			visited[start.Root] = 1
+		}
+		start.AddChildren()
+		for _, v := range start.Children { //melanjutkan DFS pada children dari node start
+			if (visited[v.Root]==0) {DLS(wg, v, append(curPath, v), depth-1, goal, found)}
+			if (found == boolHelper(true)) {
+				break
+			}
 		}
 	}
-	return false
 }
 
 func IDS(goal string) {
+	runtime.GOMAXPROCS(5)
+	var wg sync.WaitGroup
 	var curPath []*TreeNode
 	curPath = append(curPath, queue[0])
 	var depth int = 1
 	start := time.Now()
+
 	for (!found) {
-		found = DFS(queue[0], curPath, depth, goal)
+		wg.Add(1)
+		go DLS(&wg, queue[0], curPath, depth, goal, &found)
 		depth++
 	}
 	end := time.Since(start)
