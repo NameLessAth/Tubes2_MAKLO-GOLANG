e41b7971ef513398e131a0ee003712242f86101b
Kiza <themightyomegagd@gmail.com>
2024-04-27 16:58:23 +0700

    feat: Handle redirect BFS, ClearVisited, ClearQueue. Chore: pisah file

---
+++ src/backend/bfs.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+var queue []*TreeNode
+
+func Enqueue(node *TreeNode) {
+	queue = append(queue, node)
+}
+
+func Dequeue() {
+	queue = queue[1:]
+}
+
+func ClearQueue() {
+	queue = queue[:0]
+}
+
+func (node *TreeNode) AddChildToQueue() {
+	// Request the HTML page.
+	var link string
+	link = "https://en.wikipedia.org/wiki/"
+	link += node.Root
+	res, err := http.Get(link)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		fmt.Printf("%s\n", link)
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+
+	// Load the HTML document
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Find the links in the main content section
+	doc.Find("#mw-content-text").Find("a").Each(func(i int, s *goquery.Selection) {
+		// For each link found, get the href attribute and text
+		link, _ := s.Attr("href")
+		// Check if the link stays within Wikipedia domain
+		if strings.HasPrefix(link, "/wiki/") && !strings.Contains(link, ":") {
+			if !node.isChild(link[6:]) {
+				child := TreeNode{Parent: node, Root: link[6:]}
+				node.Children = append(node.Children, &child)
+				Enqueue(&child)
+			}
+		}
+	})
+}
+
+func BFS(initial string, goal string) (int, int, []string, float64) {
+	// Membersihkan Map Visited dan Queue
+	ClearQueue()
+	ClearVisited()
+
+	// Memasukkan artikel awal pada queue
+	Enqueue(&TreeNode{Root: GetTitle(initial)})
+
+	// Deklarasi variabel penyimpan banyak artikel yang ditelusuri
+	var artikelDiperiksa int = 0
+
+	// Deklarasi variabel untuk menyimpan jalan yang dilalui
+	var path []string
+
+	// Mulai menghitung
+	start := time.Now()
+
+	// Proses pencarian BFS, akan dicari selama node belum ditemukan
+	for {
+		// Artikel Tujuan ditemukan!
+		if GetTitle(queue[0].Root) == goal {
+			// Menyimmpan jalan yang dilalui
+			path = queue[0].GetPath(path)
+
+			// Keluar dari while loop
+			break
+		}
+
+		// Mengecek apakah artikel sudah pernah dikunjungi
+		if visited[queue[0].Root] == 0 {
+			artikelDiperiksa++
+			visited[queue[0].Root] = 1
+			queue[0].AddChildToQueue()
+		}
+
+		// Menghapus artikel dari queue
+		Dequeue()
+	}
+
+	// Menghitung total waktu
+	end := time.Since(start).Seconds()
+
+	// Return
+	return artikelDiperiksa, len(path), path, end
+}
--- src/backend/main.go
+++ src/backend/main.go
@@ -5,21 +5,20 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"runtime"
 	"strings"
-	"sync"
-	"time"
 
 	"github.com/PuerkitoBio/goquery"
 	"github.com/rs/cors"
 )
 
+// ADT Tree untuk tiap artikel
 type TreeNode struct {
 	Parent   *TreeNode
 	Root     string
 	Children []*TreeNode
 }
 
+// Fungsi untuk menentukan apakah sebuah artikel dapat diakses dari root
 func (node *TreeNode) isChild(str string) bool {
 	for _, child := range node.Children {
 		if child.Root == str {
@@ -29,21 +28,12 @@ func (node *TreeNode) isChild(str string) bool {
 	return false
 }
 
-var queue []*TreeNode
-
-func Enqueue(node *TreeNode) {
-	queue = append(queue, node)
-}
-
-func Dequeue() {
-	queue = queue[1:]
-}
-
-func (node *TreeNode) AddChildToQueue() {
+// Handle Redirect
+func GetTitle(linkName string) string {
 	// Request the HTML page.
 	var link string
 	link = "https://en.wikipedia.org/wiki/"
-	link += node.Root
+	link += linkName
 	res, err := http.Get(link)
 	if err != nil {
 		log.Fatal(err)
@@ -61,18 +51,35 @@ func (node *TreeNode) AddChildToQueue() {
 	}
 
 	// Find the links in the main content section
-	doc.Find("#mw-content-text").Find("a").Each(func(i int, s *goquery.Selection) {
-		// For each link found, get the href attribute and text
-		link, _ := s.Attr("href")
-		// Check if the link stays within Wikipedia domain
-		if strings.HasPrefix(link, "/wiki/") && (!strings.HasPrefix(link, "/wiki/Category:") && !strings.HasPrefix(link, "/wiki/Template:") && !strings.HasPrefix(link, "/wiki/Special:") && !strings.HasSuffix(link, ".jpg") && !strings.HasSuffix(link, ".svg") && !strings.HasSuffix(link, ".png")) {
-			if !node.isChild(link[6:]) {
-				child := TreeNode{Parent: node, Root: link[6:]}
-				node.Children = append(node.Children, &child)
-				Enqueue(&child)
-			}
-		}
-	})
+	var temp string = doc.Find("title").Text()
+	idEnd := strings.LastIndex(temp, " - Wikipedia")
+	return temp[0:idEnd]
+}
+
+// Fungsi untuk mengecek apakah link yang diinputkan merupakan link yang valid
+func IsTitleValid(linkName string) bool {
+	// Request the HTML page.
+	var link string
+	link = "https://en.wikipedia.org/wiki/"
+	link += linkName
+	res, err := http.Get(link)
+	if err != nil {
+		return false
+	}
+	defer res.Body.Close()
+	return true
+}
+
+// Fungsi untuk mendapatkan artikel yang dilalui
+func (node *TreeNode) GetPath(path []string) []string {
+	if node.Parent != nil {
+		path = node.Parent.GetPath(path)
+		path = append(path, node.Root)
+		return path
+	} else {
+		path = append(path, node.Root)
+		return path
+	}
 }
 
 func (node *TreeNode) AddChildren() {
@@ -101,133 +108,22 @@ func (node *TreeNode) AddChildren() {
 		// For each link found, get the href attribute and text
 		link, _ := s.Attr("href")
 		// Check if the link stays within Wikipedia domain
-		if strings.HasPrefix(link, "/wiki/") && (!strings.HasPrefix(link, "/wiki/Category:") && !strings.HasPrefix(link, "/wiki/Template:") && !strings.HasPrefix(link, "/wiki/Special:") && !strings.HasSuffix(link, ".jpg") && !strings.HasSuffix(link, ".svg") && !strings.HasSuffix(link, ".png")) {
+		if strings.HasPrefix(link, "/wiki/") && !strings.Contains(link, ":") {
+			var title string = GetTitle(link[6:])
 			if !node.isChild(link[6:]) {
-				child := TreeNode{Parent: node, Root: link[6:]}
+				child := TreeNode{Parent: node, Root: title}
 				node.Children = append(node.Children, &child)
 			}
 		}
 	})
 }
 
-func GetTitle(linkName string) string {
-	// Request the HTML page.
-	var link string
-	link = "https://en.wikipedia.org/wiki/"
-	link += linkName
-	res, err := http.Get(link)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer res.Body.Close()
-	if res.StatusCode != 200 {
-		fmt.Printf("%s\n", link)
-		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
-	}
-
-	// Load the HTML document
-	doc, err := goquery.NewDocumentFromReader(res.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Find the links in the main content section
-	var temp string = doc.Find("title").Text()
-	idEnd := strings.LastIndex(temp, " - Wikipedia")
-	return temp[0:idEnd]
-
-}
-
-func (node *TreeNode) GetPath(path []string) []string {
-	if node.Parent != nil {
-		path = node.Parent.GetPath(path)
-		path = append(path, node.Root)
-		return path
-	} else {
-		path = append(path, node.Root)
-		return path
-	}
-}
-
-var found bool = false
 var visited = make(map[string]int)
 
-func BFS(goal string) {
-	start := time.Now()
-	for !found {
-		if queue[0].Root == goal {
-			var path []string
-			path = queue[0].GetPath(path)
-
-			var i int = 1
-			fmt.Println("Path :")
-			for _, page := range path {
-				fmt.Printf("%d. %s\n", i, GetTitle(page))
-				i++
-			}
-			found = true
-		}
-		if visited[queue[0].Root] == 0 {
-			visited[queue[0].Root] = 1
-			queue[0].AddChildToQueue()
-		}
-		Dequeue()
+func ClearVisited() {
+	for key := range visited {
+		delete(visited, key)
 	}
-	end := time.Since(start)
-	fmt.Printf("Time elapsed: %s", end)
-}
-
-func boolHelper(b bool) *bool {
-	return &b
-}
-
-func DLS(wg *sync.WaitGroup, start *TreeNode, curPath []*TreeNode, depth int, goal string, found *bool) {
-	defer wg.Done()
-
-	if start.Root == goal { //menemukan goal
-		var path []string
-		path = start.GetPath(path)
-
-		var i int = 1
-		fmt.Println("Path :")
-		for _, page := range path {
-			fmt.Printf("%d. %s\n", i, GetTitle(page))
-			i++
-		}
-		found = boolHelper(true)
-	} else if depth <= 0 { //sudah mencapai kedalaman maksimum tetapi tidak menemukan goal
-		found = boolHelper(false)
-	} else { //lanjutkan pencarian
-		if visited[start.Root] == 0 { //menandai current node sudah dikunjungi
-			visited[start.Root] = 1
-		}
-		start.AddChildren()
-		for _, v := range start.Children { //melanjutkan DFS pada children dari node start
-			if visited[v.Root] == 0 {
-				DLS(wg, v, append(curPath, v), depth-1, goal, found)
-			}
-			if found == boolHelper(true) {
-				break
-			}
-		}
-	}
-}
-
-func IDS(goal string) {
-	runtime.GOMAXPROCS(5)
-	var wg sync.WaitGroup
-	var curPath []*TreeNode
-	curPath = append(curPath, queue[0])
-	var depth int = 1
-	start := time.Now()
-
-	for !found {
-		wg.Add(1)
-		go DLS(&wg, queue[0], curPath, depth, goal, &found)
-		depth++
-	}
-	end := time.Since(start)
-	fmt.Printf("Time elapsed: %s", end)
 }
 
 // func queryHandler(w http.ResponseWriter, r *http.Request) {
